2dd00a341023a86434663a68d2b05fbe4cb4274a
Kevin Molina <kevinmolina.dev@gmail.com>
2024-05-27 10:13:54 -0400

    Fix: Atributes the enumerable in section

--- src/data/mongo/models/section.ts
+++ src/data/mongo/models/section.ts
@@ -30,7 +30,7 @@ const SectionSchema: Schema = new Schema({
 			},
 			type: {
 				type: String,
-				enum: ["text", "number", "date", "datetime", "time", "checkbox"],
+				enum: ["text", "number", "date", "datetime", "time", "checkbox", "file"],
 				required: true,
 			},
 		},
